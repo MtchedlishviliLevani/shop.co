39d37f43f463311f16dd7c07d93d3b65e3c96cad
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-04-26 23:09:31 +0400

    configure initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import { createRoot } from 'react-dom/client'
+import Category from './pages/Category.tsx'
+import Product from './pages/Product.tsx'
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />,// Layout
+  children: [{
+    index: true,
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/category",
+    element: <Category />,
+  },
+  {
+    path: "product/:productId",
+    element: <Product />,
+    loader: async ({ params }) => {
+      return params.productId
+    }
+  }]
+}])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
+
+
