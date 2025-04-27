6e5824a81479d6634a0b37330282aa1c3b8e251f
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-04-27 23:43:14 +0400

    add responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,13 @@ import cart from "../assets/icons/cart.svg"
 import user from "../assets/icons/user.svg"
 import MobileMenu from "./MobileMenu"
 import { useState } from "react"
+import NavItem from "./NavItem"
 
 function Header() {
     const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+    function closeBurger() {
+        setIsBurgerOpen(false);
+    }
     function openBurger() {
         setIsBurgerOpen(true);
     }
@@ -15,25 +19,32 @@ function Header() {
         <header>
             <NotificationBar />
             <div className="global-container">
-                <div className="flex justify-between py-6 items-center">
-                    <div className="flex items-center gap-[18px]">
-                        <img src={menu} onClick={openBurger} alt="menu" className="cursor-pointer" />
-                        <span className="font-[Fontspring] text-[25px] mt-[-3px]">SHOP.CO</span>
+                <div className="flex justify-between py-6 items-center xl:gap-10 xl:justify-start">
+                    <div className="flex items-center gap-[18px]  xl:self-start">
+                        <img src={menu} onClick={openBurger} alt="menu" className="cursor-pointer xl:hidden" />
+                        <span className="font-[Fontspring] text-[25px] mt-[-3px] xl:text-[32px]">SHOP.CO</span>
+                    </div>
+                    <div className="hidden xl:flex flex-1 justify-center gap-10 items-center xl:self-start">
+                        <div className="xl:flex xl:gap-6 "><NavItem /></div>
+                        <div className="flex bg-Tertiary rounded-[62px] py-3 pl-4 gap-3 items-center max-w-[577px] w-[90%] xl:self-start xl:max-w-[577px]">
+                            <img src={search} alt="search" />
+                            <input className="w-full outline-none" placeholder="Search for products..." /></div>
                     </div>
 
-                    <div className="flex gap-[12px] items-center">
-                        <img src={search} alt="search" className="" />
+                    <div className="flex gap-[12px] items-center xl:self-start xl:mt-2">
+                        <img src={search} alt="search" className="xl:hidden" />
                         <img src={cart} alt="cart" className="" />
                         <img src={user} alt="user" className="" />
                     </div>
 
                 </div>
             </div>
-            <nav>
+            <nav className="">
 
                 <ul>
                     {isBurgerOpen &&
-                        <MobileMenu />}
+                        <MobileMenu onCloseBurger={closeBurger} />}
+
                 </ul>
 
             </nav>
